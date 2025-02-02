d62871702785a83aee209eb6b8da5e4045ee21a9
Ankur Chauhan <115866837+ankurchauhanOK@users.noreply.github.com>
2025-02-02 13:28:56 +0530

    first commit

---
+++ src/components/Portfolio.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Portfolio = () => {
+    return (
+        <>
+            <div className="my-4 py-4" id='portfolio'>
+                <h2 className="my-2 text-center text-3xl text-blue-900 uppercase font-bold">Products</h2>
+                <div className='flex justify-center'>
+                    <div className='w-24 border-b-4 border-blue-900 mb-8'></div>
+                </div>
+
+                <div className="px-4" data-aos="fade-down" data-aos-delay="600">
+                    <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-5">                            
+                        <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3 min-h-max">
+                            <div className="m-2 text-justify text-sm">
+                                <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">EtherEase</h4>
+                                <p className="text-md font-medium leading-5 h-auto md:h-48">
+                                EtherEase is committed to simplifying Web 3.0 adoption for non-technical users and freelancing teams by offering 
+                                a user-friendly platform, enhanced security, and a simplified tokenization process. Through collaborative 
+                                problem-solving and rigorous testing, we've successfully overcome challenges to provide a seamless and secure 
+                                experience for our users.
+                                </p>
+                                <div className="flex justify-center my-4">
+                                    <Link to="/get-demo" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl">
+                                        Schedule Demo
+                                        <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="bg-white transition-all ease-in-out duration-400 overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3">
+                            <div className="m-2 text-justify text-sm">
+                                <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">School Management Portal</h4>
+                                <p className="text-md font-medium leading-5 h-auto md:h-48">
+                                    Our all encompassing School Management Portal is the only solution needed for any institution.
+                                    The School Management Portal (SMP) is a tool that can help educational institutions of all kinds 
+                                    manage their administrative tasks, automate processes, and streamline communication between teachers, students, parents, and administrators. 
+                                    The software can be used to manage all aspects of school operations, including student enrollment, attendance, grades, schedules, and more.
+                                </p>
+                                <div className="flex justify-center my-4">
+                                    <Link to="/get-demo" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl">
+                                        Schedule Demo
+                                        <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                                    </Link>
+                                </div>
+                                
+                                {/* <ul className="border-t border-gray-300 py-8 space-y-6">
+                                    <li className="flex items-center space-x-2 px-8">
+                                        <span className="bg-blue-600 rounded-full p-1">
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3 text-white" viewBox="0 0 20 20" fill="currentColor">
+                                                <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
+                                            </svg>
+                                        </span>
+                                        <span className="text-gray-600 capitalize">Free Setup Guidance</span>
+                                    </li>
+                                </ul> */}
+                            </div>
+                        </div>
+
+                        <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3">
+                            <div className="m-2 text-justify text-sm">
+                            <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12"> GDSC ABESIT</h4>
+                                <p className="text-md font-medium leading-5 h-auto md:h-48">
+                                The GDSC ABESIT platform is a dynamic hub designed to empower students with cutting-edge tech knowledge, 
+                                hands-on projects, and a collaborative learning environment. It connects aspiring developers, designers, 
+                                and tech enthusiasts to industry trends, workshops, hackathons, and mentorship opportunities.
+                                </p>
+                                <div className="flex justify-center my-4">
+                                    <Link to="/get-demo" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl">
+                                        Schedule Demo
+                                        <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3">
+                            <div className="m-2 text-justify text-sm">
+                                <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">Event Management System</h4>
+                                <p className="text-md font-medium leading-5 h-auto md:h-48">
+                                    Our event management system helps to manage different types of events, whether weddings, burials or any kind of event. With an easily customizable menu, you get to define your user experience and make your event fully customizable and memorable.
+                                    With use cases already recorded for quite a number of events, we can assure you of a stress-less event mangement platform.
+                                </p>
+                                <div className="flex justify-center my-4">
+                                    <Link to="/get-demo" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl">
+                                        Schedule Demo
+                                        <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>                    
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Portfolio;
